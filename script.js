fd59debcea3ce0e2543fc0adf002c397bec00df2
Anton Minchonak <anton.minchonak@gmail.com>
2021-12-17 01:54:08 +0300

    adaptive stats upgrade

--- script.js
+++ script.js
@@ -952,74 +952,82 @@ let hintData = [
 hintsForHeroes()
 
 
+function adaptiveTools() {
 
-let statButtons = document.querySelectorAll(".match-info__navbar-button");
-let temp = document.querySelector(".navbar-button--active");
+  let statButtons = document.querySelectorAll(".match-info__navbar-button");
+  let previousButton = statButtons[0];
 
-function hideStats(from, to) {
-  for (let i = from; i < to; i++) {
-    for (let j = 0; j < 14; j++) {
-      document.querySelectorAll(
-        `.match-info__statsblock-row>div:nth-child(${i})`
-      )[j].style.display = "none";
+  function hideStats(from, to) {
+    for (let i = from; i < to; i++) {
+      for (let j = 0; j < 14; j++) {
+        document.querySelectorAll(
+          `.match-info__statsblock-row>div:nth-child(${i})`
+        )[j].style.display = "none";
+      }
     }
   }
-}
 
-function openStats(from, to) {
-  for (let i = from; i < to; i++) {
-    for (let j = 0; j < 14; j++) {
-      document.querySelectorAll(
-        `.match-info__statsblock-row>div:nth-child(${i})`
-      )[j].style.display = "block";
+  function openStats(from, to) {
+    for (let i = from; i < to; i++) {
+      for (let j = 0; j < 14; j++) {
+        document.querySelectorAll(
+          `.match-info__statsblock-row>div:nth-child(${i})`
+        )[j].style.display = "block";
+      }
     }
   }
-}
 
-statButtons[0].onclick = () => {
-  hideStats(9, 20);
-  openStats(5, 9);
-}
+  statButtons[0].onclick = () => {
+    hideStats(9, 20);
+    openStats(5, 9);
+    statButtons[0].classList.add("navbar-button--active");
+    previousButton.classList.remove("navbar-button--active");
+  }
 
-statButtons[1].onclick = () => {
- hideStats(5, 9);
-   hideStats(15, 20);
-   openStats(9, 15);
-    }
+  statButtons[1].onclick = () => {
+    hideStats(5, 9);
+    hideStats(15, 20);
+    openStats(9, 15);
+    statButtons[1].classList.add("navbar-button--active");
+    previousButton.classList.remove("navbar-button--active");
+    previousButton = statButtons[1];
+  }
 
 
-statButtons[2].onclick = () => {
-       hideStats(5, 15);
-       hideStats(18, 20);
-       openStats(15, 18);
-};
+  statButtons[2].onclick = () => {
+    hideStats(5, 15);
+    hideStats(18, 20);
+    openStats(15, 18);
+    statButtons[2].classList.add("navbar-button--active");
+    previousButton.classList.remove("navbar-button--active");
+    previousButton = statButtons[2];
+  };
     
 
- statButtons[3].onclick = () => {
-   hideStats(5, 18);
-   openStats(18, 20);
- };
+  statButtons[3].onclick = () => {
+    hideStats(5, 18);
+    openStats(18, 20);
+    statButtons[3].classList.add("navbar-button--active");
+    previousButton.classList.remove("navbar-button--active");
+    previousButton = statButtons[3];
+  };
 
+  let columnsForAdaptive = document.querySelectorAll(`.match-info__statsblock-row>div`);
 
-//window.onresize = () => {
-//  for (let i = 9; i < 20; i++) {
-//    for (let j = 0; j < 14; j++) {
-//      let columnToHide = document.querySelectorAll(
-//        `.match-info__statsblock-row>div:nth-child(${i})`
-//      )[j];
-//      if (window.innerWidth < 800) {
-//
-//        if (!columnToHide.classList.contains("hidden")) {
-//         console.log(!columnToHide.classList.contains("hidden"));
-//          columnToHide.classList.add("hidden");
-//        }
-//      } else if (window.innerWidth > 800 ) {
-//        columnToHide.classList.remove("hidden");
-//      }
-//    }
-//  }
-//};
+  window.onresize = () => {
+    if (window.innerWidth > 800) {
+      for (let i = 1; i < columnsForAdaptive.length; i++) {
+        columnsForAdaptive[i].style.display = "";
+      }
+      previousButton = statButtons[0];
+      if (!statButtons[0].classList.contains('navbar-button--active')) {
+        statButtons[0].classList.add("navbar-button--active");
+        statButtons[1].classList.remove("navbar-button--active");
+        statButtons[2].classList.remove("navbar-button--active");
+        statButtons[3].classList.remove("navbar-button--active");
+      }
+    }
+  };
+}
 
-
-
-
+adaptiveTools();
